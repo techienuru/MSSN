a2013e63f43fa861c857523ca4a9ea12c8e5bebd
Ibrahim Nurudeen shehu <ibrahimnurudeenshehu1447@gmail.com>
2024-08-30 18:31:27 +0100

    coded event and blog details page

---
+++ includes/pages/footer.php
@@ -0,0 +1,54 @@
+ <!-- Footer -->
+ <footer>
+     <div class="container pb-1 pt-3">
+         <div class="row row-cols-md-3">
+             <div class="col-sm">
+                 <a href="" class="navbar-brand d-flex gap-2">
+                     <img
+                         src="./images/logo.png"
+                         class="img-fluid d-inline"
+                         width="40"
+                         height="30"
+                         alt="logo" />
+                     <div
+                         class="d-flex flex-column justify-content-center text-dark"
+                         style="font-size: 10px">
+                         <span>Muslim Student Society Of Nigeria</span>
+                         <span>NSUK Chapter</span>
+                     </div>
+                 </a>
+                 <p class="mt-2 mb-0">MSSN NSUK Chapter.... hope of the ummah</p>
+                 <p class="m-0">mssn@gmail.com</p>
+                 <p>+234 8062 5856 24</p>
+             </div>
+             <div class="col">
+                 <h5>Follow Us</h5>
+                 <div class="d-flex gap-3">
+                     <a href="">
+                         <i class="fa-brands fa-facebook"></i>
+                     </a>
+                     <a href="">
+                         <i class="fa-brands fa-youtube"></i>
+                     </a>
+                     <a href="">
+                         <i class="fa-brands fa-whatsapp"></i>
+                     </a>
+                 </div>
+             </div>
+             <div class="col">
+                 <h5>Quick Links</h5>
+                 <ul class="row row-cols-2 justify-content-end">
+                     <a href="./index.html" class="nav-link">Home</a>
+                     <a href="" class="nav-link">About</a>
+                     <a href="./library.html" class="nav-link">Library & Resources</a>
+                     <a href="./excos.html" class="nav-link">Excos</a>
+                     <a href="./blog.html" class="nav-link">Blog & Event</a>
+                     <a href="#" class="nav-link">#To top</a>
+                 </ul>
+             </div>
+         </div>
+         <p class="text-center mt-3">
+             &copy; MSSN NSUK Chapter 2024| All rights reserved.
+         </p>
+     </div>
+ </footer>
---
+++ includes/pages/navbar.php
@@ -0,0 +1,76 @@
+<!-- Start of Navbar -->
+<section class="navbar navbar-expand-md" id="navbar-section">
+    <div class="container">
+        <a href="" class="navbar-brand d-flex gap-2">
+            <img
+                src="./images/logo.png"
+                class="img-fluid d-inline"
+                width="40"
+                height="30"
+                alt="logo" />
+            <div
+                class="d-flex flex-column justify-content-center text-white"
+                style="font-size: 10px">
+                <span>Muslim Student Society Of Nigeria</span>
+                <span>NSUK Chapter</span>
+            </div>
+        </a>
+
+        <!-- Hamburger -->
+        <button class="hamburger">
+            <div></div>
+            <div></div>
+            <div></div>
+        </button>
+
+        <ul class="navbar-nav collapse navbar-collapse justify-content-end">
+            <li class="nav-item">
+                <a href="./index.html" class="nav-link active">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">About</a>
+            </li>
+            <li class="nav-item">
+                <a href="./library.html" class="nav-link">Library & Resources</a>
+            </li>
+            <li class="nav-item">
+                <a href="./excos.html" class="nav-link">Excos</a>
+            </li>
+            <li class="nav-item">
+                <a href="./blog.html" class="nav-link">Blog & Event</a>
+            </li>
+        </ul>
+    </div>
+</section>
+
+<div class="hamburger-menu px-5 py-3">
+    <div class="d-flex justify-content-between align-items-center mb-5">
+        <a href="" class="">
+            <img
+                src="./images/logo.png"
+                class="img-fluid"
+                width="40"
+                height="30"
+                alt="logo" />
+        </a>
+        <button type="button" class="btn-close js-hamburger-close"></button>
+    </div>
+    <ul>
+        <li>
+            <a href="./index.html">Home</a>
+        </li>
+        <li>
+            <a href="">About</a>
+        </li>
+        <li>
+            <a href="./library.html">Library & Resources</a>
+        </li>
+        <li>
+            <a href="./excos.html">Our Excos</a>
+        </li>
+        <li>
+            <a href="./blog.html">Blog & Event</a>
+        </li>
+    </ul>
+</div>
+<!-- End of Navbar -->
