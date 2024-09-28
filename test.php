1c5783f179d9d403e8d069609ba4ebe612c40959
Ibrahim Nurudeen shehu <ibrahimnurudeenshehu1447@gmail.com>
2024-09-28 17:39:55 +0100

    testing connection to db

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+$connect = new mysqli('localhost', 'mssnnsuk_chapter', 'mssnnsukchapter', 'mssnnsuk_chapter'); //Server name,Username,Password, and Database Name
+
+if (!$connect) {
+    die("<b>Error While Connecting to Database</b>: " . $connect->errorno);
+}
