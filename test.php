679086f9eefd4cc255e0f40ac04e58a9456faf07
Ibrahim Nurudeen shehu <ibrahimnurudeenshehu1447@gmail.com>
2024-09-28 18:12:26 +0100

    still testing db connection

--- test.php
+++ test.php
@@ -5,3 +5,18 @@
 //     die("<b>Error While Connecting to Database</b>: " . $connect->errorno);
 // }
 echo $_SERVER["REMOTE_ADDR"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Welcome</h1>
+</body>
+
+</html>
